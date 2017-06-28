57e8dd0962565ad0cd823377d40dc80f9996eabf
Jacky Wang <www.wangzixiao@gmail.com>
2017-06-28 16:40:08 +0800

    Compiled index.js

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
+"use strict";
+/// <reference path="./node_modules/typescript/lib/lib.es2016.d.ts" />
+/// <reference path="./node_modules/typescript/lib/lib.dom.d.ts" />
+Object.defineProperty(exports, "__esModule", { value: true });
+var translate_module_1 = require("./src/translate.module");
+exports.TranslateModule = translate_module_1.TranslateModule;
+var translate_service_1 = require("./src/translate.service");
+exports.TranslateService = translate_service_1.TranslateService;
 //# sourceMappingURL=index.js.map
