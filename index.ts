11d2ad0ce0c4b0ba030a1d2e94e266afbc379754
Jacky Wang <www.wangzixiao@gmail.com>
2017-07-26 17:51:24 +0800

    Fixed bugs

--- index.ts
+++ index.ts
@@ -1,5 +1,2 @@
-import { TranslateModule } from './src/translate.module';
-import { TranslateService } from './src/translate.service';
-
-export TranslateModule;
-export TranslateService;
+export TranslateModule from './src/translate.module';
+export TranslateService from './src/translate.service';
